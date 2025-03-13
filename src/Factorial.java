8c23dea81ee22fdb01b91093f88f3e5fd9a0fa04
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-03-13 18:21:58 +0000

    Factorial

---
+++ src/Factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static int fact(int n) {
+        if (n >= 1) {
+            return n * fact(n - 1);
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Factorial of " + number + " is " + fact(number));
+
+        scanner.close();
+    }
+}
